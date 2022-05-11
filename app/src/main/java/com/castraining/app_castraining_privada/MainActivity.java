8a784c3bb87ba6f4fcf3b4739fd32d33d416d69e
logan <artemipastor@gmail.com>
2022-05-11 16:17:35 +0200

    Git ini

---
+++ app/src/main/java/com/castraining/app_castraining_privada/MainActivity.java
@@ -0,0 +1,14 @@
+package com.castraining.app_castraining_privada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
